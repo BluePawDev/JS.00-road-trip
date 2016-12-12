1a2b73b6d7c03f9cdb3b1e71fd1ac02d0118f9ed
ja-ruby <jaruby@me.com>
2016-12-12 15:52:12 -0600

    Added "sheep" to output string

--- scripts/application.js
+++ scripts/application.js
@@ -22,7 +22,7 @@ var numSheep = 4;
 var monthNumber = 1;
 var monthsToPrint = 12;
 while (monthNumber <= monthsToPrint) {
-	numSheep = numSheep * 4;
-  console.log("There will be " + numSheep + " after " + monthNumber + " month(s)!");
+	numSheep *= 4;
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
   monthNumber++;
 }
