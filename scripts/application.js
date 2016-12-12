0e8d7f2ac8cf6360dce05f900b4ae0b645684ae7
ja-ruby <jaruby@me.com>
2016-12-12 15:33:31 -0600

    While loop practice
    Another while loop test…successful:
    var trainNumber = 1;
    while (trainNumber <= 8) {
      console.log("Train #" + trainNumber + " is running.");
      trainNumber++;
    }

--- scripts/application.js
+++ scripts/application.js
@@ -3,3 +3,9 @@ while (myNumber <= 5) {
   console.log(myNumber);
   myNumber++;
 }
+
+var trainNumber = 1;
+while (trainNumber <= 8) {
+  console.log("Train #" + trainNumber + " is running.");
+  trainNumber++;
+}
