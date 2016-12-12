b8c1096b42c76423aaa07c824019c13322663d1e
ja-ruby <jaruby@me.com>
2016-12-12 15:50:04 -0600

    Another while loop
    var numSheep = 4;
    var monthNumber = 1;
    var monthsToPrint = 12;
    while (monthNumber <= monthsToPrint) {
    	numSheep = numSheep * 4;
      console.log("There will be " + numSheep + " after " + monthNumber + "
    month(s)!");
      monthNumber++;
    }

--- scripts/application.js
+++ scripts/application.js
@@ -17,3 +17,12 @@ while (num >= 1) {
   console.log(num);
   num--;
 }
+
+var numSheep = 4;
+var monthNumber = 1;
+var monthsToPrint = 12;
+while (monthNumber <= monthsToPrint) {
+	numSheep = numSheep * 4;
+  console.log("There will be " + numSheep + " after " + monthNumber + " month(s)!");
+  monthNumber++;
+}
