20eae0c7023f86e45a96f916f4c68bc9c3744f01
ja-ruby <jaruby@me.com>
2016-12-16 11:56:53 -0600

    Functions
    Beginning work on JS functions

--- scripts/application.js
+++ scripts/application.js
@@ -178,11 +178,13 @@
 // var ageIsCorrect = confirm("You entered " + userAge + ". Is this correct?");
 
 // 2
-var userAge = prompt("What's your age, user?");
-var ageIsCorrect = false;
-while(ageIsCorrect === false){
-  if(confirm("You entered  " + userAge + ". Is this correct?")){
-    alert("Great! Your age is logged as " + userAge + ".");
-    ageIsCorrect = true;
-  }
-}
+// var userAge = prompt("What's your age, user?");
+// var ageIsCorrect = false;
+// while(ageIsCorrect === false){
+//   if(confirm("You entered  " + userAge + ". Is this correct?")){
+//     alert("Great! Your age is logged as " + userAge + ".");
+//     ageIsCorrect = true;
+//   }
+// }
+
+//FUNCTIONS
