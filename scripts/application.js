b1796146640ee7ee851e48c25479d73f263443ba
ja-ruby <jaruby@me.com>
2016-12-17 10:57:24 -0600

    Function Practice

--- scripts/application.js
+++ scripts/application.js
@@ -270,20 +270,62 @@
 // More Comples Functions
 // Function that counts the "E's" from a user-entered phrase
 
-countE();
+// countE();
+//
+// function countE(){
+//   var phrase = prompt("Which phrase would you like to examine?");
+//   if(typeof(phrase) !== "string"){
+//     alert("That's not a valid entry!");
+//     return false;
+//   } else {
+//     var eCount = 0;
+//     for(var index = 0; index < phrase.length; index++){
+//       if(phrase.charAt(index) === "e" || phrase.charAt(index) === "E")
+//       eCount++;
+//       }
+//     alert("There are " + eCount + " e's in the phrase \"" + phrase + "\".");
+//     return true;
+//     }
+//   }
 
-function countE(){
-  var phrase = prompt("Which phrase would you like to examine?");
-  if(typeof(phrase) !== "string"){
-    alert("That's not a valid entry!");
-    return false;
-  } else {
-    var eCount = 0;
-    for(var index = 0; index < phrase.length; index++){
-      if(phrase.charAt(index) === "e" || phrase.charAt(index) === "E")
-      eCount++;
-      }
-    alert("There are " + eCount + " e's in the phrase \"" + phrase + "\".");
-    return true;
-    }
+// Function Practice
+// The Park Rangers in Death Valley National Park divide up the feed responsibilities daily for the Bighorn Sheep population. Each sheep needs 2 lbs of ranger-provided food per day.
+// Build a function called feedPerRanger that takes in:
+//   The current population of sheep.
+//   The number of Park Rangers available during the day.
+// The function should alert the amount of feed that each Park Ranger should be responsible for on that day. The output should match the following format: "Each Park Ranger should load <number> lbs of feed today."
+
+// feedPerRanger(1200,  10);
+// function feedPerRanger(numSheep, numRgrs) {
+//   var lbsFood = (numSheep * 2) / numRgrs;
+//   alert("Each Park Ranger should load " + lbsFood + " lbs of feed today.");
+// }
+
+
+
+// Technicians have decided they want more control of which generators are on and off.
+// They’ve asked you to develop a way to adjust the total megawatts generated upon the switch-on or switch-off of a single, chosen generator.
+// Build a function named changePowerTotal that takes in four parameters:
+//   The total power generated (a number)
+//   The generator ID for the current generator (a number)
+//   The generator status (a string that says "on" or "off")
+//   The amount of power produced by the current generator (a number)
+// Your function should:
+//   If the current generator is set to "on", then add to the total power generated.
+//   Or if the current generator is set to "off", then remove from the total power generated.
+//   return the total power generated.
+//   alert the technician in the following formats:
+// For switching on: "Generator #2 is now on, adding 62 MW, for a total of 62 MW!""
+// For switching off: "Generator #2 is now off, removing 62 MW, for a total of 0 MW!""
+// Note: You do not need to call the function. Build the function declaration without invoking it.
+
+function changePowerTotal(totalMW, currentGenID, currentGenStatus, currentGenMW){
+  if(currentGenStatus === "on"){
+    totalMW += currentGenMW;
+    alert("Generator #" + currentGenID + " is now on, adding " + currentGenMW + " MW, for a total of " + totalMW + " MW !");
+  } else if (currentGenStatus === "off") {
+    totalMW -= currentGenMW;
+    alert("Generator #" + currentGenID + " is now off, removing " + currentGenMW + " MW, for a total of " + totalMW + " MW!");
   }
+  return totalMW;
+}
