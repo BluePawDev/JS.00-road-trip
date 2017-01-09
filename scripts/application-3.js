f24feac74fa98302f0c9dd0be093ef87bd29488c
ja-ruby <jaruby@me.com>
2017-01-09 13:45:14 -0600

    Anon. Function

--- scripts/application-3.js
+++ scripts/application-3.js
@@ -50,17 +50,20 @@
 // fearGenerated(people, rain, sharks);
 // alert(fearGenerated);
 
+//fearGenerated(3,4,7);
 
 var fear = fearGenerated(numPeeps, rainInInches, numSharks);
 
 var fearMessage = function() {
-  // Insert conditional statements here
-
+  if (fear < 200) {
+    return confirm("Fear Level: LOW\nStill wanna ride?");
+  } else if (fear >= 200 && fear <= 300) {
+    return confirm("Fear Level: MEDIUM\nThink you'll make it?");
+  }
 };
 
 function confirmRide(confirmToGo) {
   return confirmToGo();
 }
 
-// Call confirmRide with the fearMessage function
 var startRide = confirmRide(fearMessage);
